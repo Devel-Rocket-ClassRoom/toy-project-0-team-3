83be83e27b2431a51c1ca3c5674babf6fdf3fc45
김유찬 <152022287+muchan918@users.noreply.github.com>
2026-05-02 15:59:25 +0900

    feat skill 추가

--- Assets/Scripts/Player/PlayerSkill.cs
+++ Assets/Scripts/Player/PlayerSkill.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerSkill : MonoBehaviour
 {
     public GameObject Skills;
+    private Animator _animator;
 
     private PlayerInput _input;
     private QSkill _qSkill;
@@ -13,6 +14,7 @@ public class PlayerSkill : MonoBehaviour
     private void Awake()
     {
         _input = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
         _qSkill = Skills.GetComponent<QSkill>();
         _wSkill = Skills.GetComponent<WSkill>();
         _eSkill = Skills.GetComponent<ESkill>();
@@ -21,9 +23,26 @@ public class PlayerSkill : MonoBehaviour
 
     private void Update()
     {
-        if (_input.SkillQ) _qSkill?.Use();
-        if (_input.SkillW) _wSkill?.Use();
-        if (_input.SkillE) _eSkill?.Use();
-        if (_input.SkillR) _rSkill?.Use();
+        if (_input.SkillQ)
+        {
+            _qSkill?.Use();
+            _animator.SetTrigger("QSkill");
+        }
+        if (_input.SkillW)
+        {
+            _wSkill?.Use();
+            _animator.SetTrigger("WSkill");
+        }
+
+        if (_input.SkillE)
+        {
+            _eSkill?.Use();
+            _animator.SetTrigger("ESkill");
+        }
+        if (_input.SkillR)
+        {
+            _rSkill?.Use();
+            _animator.SetTrigger("RSkill");
+        }
     }
 }
